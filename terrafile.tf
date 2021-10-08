486c988903ac60afa8f1150982c693e451269f88
enzziom <enzziom@gmail.com>
2021-10-08 17:09:48 -0300

    we need the new vpc

--- terrafile.tf
+++ terrafile.tf
@@ -6,12 +6,13 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "metal.corp-devops-test"
-    key    = "factoryfloor-terraform-.tfstate"
+    key    = "infra/factoryfloor-terraform-.tfstate"
     region = "us-east-2"
   }
 }
 
 module "dev_cluster" {
-  source        = "git@github.com:EzzioMoreira/module-factory-floor.git?ref=main"
-  environment   = "development"
+  source         = "git@github.com:EzzioMoreira/module-factory-floor.git?ref=main"
+  environment    = "development"
+  vpc_cidr_block = "10.1.0.0/16"
 }
